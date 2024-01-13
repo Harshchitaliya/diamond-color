f9c8d59cf7288a3d0b6680b1f40523350141a3cb
Harsh chitaliya <98301276+Harshchitaliya@users.noreply.github.com>
2024-01-13 14:17:26 +0530

    modify a make_dataset and add params.yaml

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,58 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import numpy as np
+import pathlib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+import yaml
+
+# load data
+def load_dataset(datapath):
+    df = pd.read_csv(datapath)
+    return df
+
+# split data into train-test
+def data_split(df,test_split,seed):
+    X=df.iloc[:,170:330]
+    Y=df["color"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_split, random_state=seed)
+
+    le=LabelEncoder()
+    le.fit(y_train)
+    y_train=pd.DataFrame(le.transform(y_train))
+    y_test=pd.DataFrame(le.transform(y_test))
+
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train = pd.DataFrame(scaler.transform(X_train))
+    X_test = pd.DataFrame(scaler.transform(X_test))
+
+    return X_train, X_test, y_train, y_test
+
+# save data
+def save_data(X_train, X_test, y_train, y_test,output_path):
+    pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
+    X_train.to_csv(output_path + 'X_train.csv', index=False)
+    X_test.to_csv(output_path + 'X_test.csv', index=False)
+    y_train.to_csv(output_path + 'y_train.csv', index=False)
+    y_test.to_csv(output_path + 'y_test.csv', index=False)
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def main():
+
+    curr_dir = pathlib.Path(__file__)
+    home_dir = curr_dir.parent.parent.parent
+    params_file = home_dir.as_posix() + '/params.yaml'
+    params = yaml.safe_load(open(params_file))["make_dataset"]
+
+    data_path = home_dir.as_posix() + "/data/build_data/df.csv"
+    output_path = home_dir.as_posix() + '/data/processed/'
+
+    df = load_dataset(data_path)
+    X_train,X_test,y_train,y_test = data_split(df, params['test_split'], params['seed'])
+    save_data(X_train,X_test,y_train,y_test, output_path)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
+if __name__ == "__main__":
     main()
